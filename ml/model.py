b487e30d2847cc31bafa97f6ccd629c1df8286fe
Astola <laura.astola@accenture.com>
2025-08-12 13:14:52 +0200

    modified model.py

--- ml/model.py
+++ ml/model.py
@@ -2,6 +2,12 @@ import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import fbeta_score, precision_score, recall_score
+from pickle import load
+from pickle import dump
+
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -20,7 +26,9 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
     # TODO: implement the function
-    pass
+    clf = MLPClassifier(random_state=1, max_iter=300).fit(X_train, y_train)
+
+    return clf
 
 
 def compute_model_metrics(y, preds):
@@ -60,7 +68,9 @@ def inference(model, X):
         Predictions from the model.
     """
     # TODO: implement the function
-    pass
+    preds = model.predict(X)
+
+    return preds
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -73,12 +83,16 @@ def save_model(model, path):
         Path to save pickle file.
     """
     # TODO: implement the function
-    pass
+    with open(path, "wb") as f:
+        dump(model, f, protocol=5)
+    return
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
     # TODO: implement the function
-    pass
+    with open(path, "rb") as f:
+        model = load(f)
+    return model
 
 
 def performance_on_categorical_slice(
@@ -118,11 +132,14 @@ def performance_on_categorical_slice(
 
     """
     # TODO: implement the function
+    data[column_name] = slice_value
     X_slice, y_slice, _, _ = process_data(
         # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
+        data, categorical_features, label, training=False, encoder=encoder, lb=lb
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    X_train, X_test, y_train, y_test = train_test_split(X_slice, y_slice, test_size=0.33, random_state=42)
+    preds = inference(model, X_test) 
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
